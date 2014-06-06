24d7b6d48a13f2fae00a0644d0851cab792fcecc
Brookins Consulting <info@brookinsconsulting.com>
2014-06-06 18:04:46 -0500

    Updated: Adding AWS S3 Rename Settings.

--- settings/s3.ini.append.php
+++ settings/s3.ini.append.php
@@ -6,5 +6,10 @@ SecretKey=mqkYzftmDxxe8ZMoQBdvAFfedUI4CcreTbSnpYYE
 Bucket=bc-web-upload
 # FileSizeLimit in KB. Default is 1972.890625 MB
 FileSizeLimit=2020240
+ParentNodeID=43
+ClassIdentifier=large_file
+AttributeIdentifier=aws_s3_upload_client
+RenameAttributeIdentifier=aws_s3_renamed
+HoursAgo=1
 
 */ ?>
