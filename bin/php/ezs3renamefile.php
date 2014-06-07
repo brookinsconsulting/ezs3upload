1c0d9934eb206f9cd99664af4e1d8ba10243d524
Brookins Consulting <info@brookinsconsulting.com>
2014-06-07 17:01:18 -0500

    Updated: Added documentation support for new s3 file object rename support

--- bin/php/ezs3renamefile.php
+++ bin/php/ezs3renamefile.php
@@ -6,7 +6,7 @@
  * @copyright Copyright (C) 1999 - 2015 Brookins Consulting. All rights reserved.
  * @copyright Copyright (C) 2013 - 2015 Think Creative. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or later)
- * @version 0.0.8
+ * @version 0.0.2
  * @package ezps3upload
  */
 
