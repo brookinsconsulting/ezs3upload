9d2914a5f41e18f9d1d59ad2dd57e851f574500e
Brookins Consulting <info@brookinsconsulting.com>
2014-06-07 12:13:46 -0500

    Updated: Added admin script user login. Refined script debug output

--- bin/php/ezs3rename.php
+++ bin/php/ezs3rename.php
@@ -6,11 +6,15 @@
  * @copyright Copyright (C) 1999 - 2015 Brookins Consulting. All rights reserved.
  * @copyright Copyright (C) 2013 - 2015 Think Creative. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or later)
- * @version 0.0.4
+ * @version 0.0.8
  * @package ezps3upload
  */
 
+/** Disable script php memory limit **/
+
 ini_set("memory_limit", -1);
+
+/** Script autoloads initialization **/
 
 require 'autoload.php';
 
@@ -27,11 +31,12 @@ $script = eZScript::instance( array( 'description' => ( "eZ Publish AWS S3 Renam
 
 $script->startup();
 
-$options = $script->getOptions( "[script-verbose:][parent-node:][hours:]",
+$options = $script->getOptions( "[script-verbose;][script-verbose-level;][parent-node:][hours:]",
                                 "[node]",
                                 array( 'parent-node' => 'Content Tree Node ID. Example: --parent-node=2',
-                                       'days' => 'Number of days to search in reverse. Example: --days=5',
-                                       'script-verbose' => 'Use this parameter to display verbose script output without disabling script iteration counting of images created or removed. Example: ' . "'--script-verbose'" . ' is an optional parameter which defaults to false'),
+                                       'hours' => 'Number of hours to search in reverse. Example: --hours=5',
+                                       'script-verbose' => 'Use this parameter to display verbose script output without disabling script iteration counting of images created or removed. Example: ' . "'--script-verbose'" . ' is an optional parameter which defaults to false',
+                                       'script-verbose-level' => 'Use only with ' . "'--script-verbose'" . ' parameter to see more of execution internals. Example: ' . "'--script-verbose-level=3'" . ' is an optional parameter which defaults to 1'),
                                 false,
                                 array( 'user' => true ) );
 $script->initialize();
@@ -40,11 +45,19 @@ $script->initialize();
 
 $limit = 100;
 $offset = 0;
+$adminUserID = 14;
 
 $fetchClassIdentifier = eZINI::instance( 's3.ini' )->variable( 'S3Settings', 'ClassIdentifier' );
-$awsS3Bucket = eZINI::instance( 's3.ini' )->variable( 'S3Settings', 'Bucket' );
 $attributeIdentifier = eZINI::instance( 's3.ini' )->variable( 'S3Settings', 'AttributeIdentifier' );
 $renameAtributeIdentifier = eZINI::instance( 's3.ini' )->variable( 'S3Settings', 'RenameAttributeIdentifier' );
+$awsS3Bucket = eZINI::instance( 's3.ini' )->variable( 'S3Settings', 'Bucket' );
+
+/** Login script to run as admin user  This is required to see past content tree permissions, sections and other limitations **/
+
+$currentuser = eZUser::currentUser();
+$currentuser->logoutCurrent();
+$user = eZUser::fetch( $adminUserID );
+$user->loginCurrent();
 
 /** Test for required script arguments **/
 
@@ -81,6 +94,10 @@ if ( !is_numeric( $hoursAgo ) )
 
 $verbose = isset( $options['script-verbose'] ) ? true : false;
 
+$scriptVerboseLevel = isset( $options['script-verbose-level'] ) ? $options['script-verbose-level'] : 1;
+
+$troubleshoot = ( isset( $options['script-verbose-level'] ) && $options['script-verbose-level'] > 0 ) ? true : false;
+
 /** Modified time stamp to search with **/
 
 $modifiedTimeStamp = time() - ( $hoursAgo * 3600 );
@@ -97,9 +114,11 @@ $subTreeCountByNodeIDParams = array( 'ClassFilterType' => 'include',
 
 $totalFileCount = eZContentObjectTreeNode::subTreeCountByNodeID( $subTreeCountByNodeIDParams, $parentNodeID );
 
+/** Debug verbose output **/
+
 if( $verbose )
 {
-    $cli->output( "Total File Object Count: " . $totalFileCount . "\n" );
+    $cli->output( "Found! Total File Objects To Be Renamed: " . $totalFileCount . "\n" );
 }
 
 if ( !$totalFileCount )
@@ -110,7 +129,7 @@ if ( !$totalFileCount )
 
 /** Alert user of script process starting **/
 
-$cli->output( "Searching through content subtree from node $parentNodeID to find S3 large file objects to be processed ...\n" );
+$cli->output( "Searching content tree from starting node '$parentNodeID' to find S3 large file objects to be processed ...\n" );
 
 /** Setup script iteration details **/
 
@@ -125,7 +144,7 @@ while ( $offset < $totalFileCount )
     $subTreeByNodeIDParams = array( 'ClassFilterType' => 'include',
                                     'ClassFilterArray' => array( $fetchClassIdentifier ),
                                     'AttributeFilter' => array( 'and', array( 'modified','>=', $modifiedTimeStamp ),
-                                                                        array( "large_file/$renameAtributeIdentifier",'!=', true ) ),
+                                                                       array( "large_file/$renameAtributeIdentifier",'!=', true ) ),
                                     'Limit', $limit,
                                     'Offset', $offset,
                                     'SortBy', array( 'modified', false ),
@@ -135,7 +154,9 @@ while ( $offset < $totalFileCount )
 
     $subTree = eZContentObjectTreeNode::subTreeByNodeID( $subTreeByNodeIDParams, $parentNodeID );
 
-    if( $verbose )
+    /** Optional debug output **/
+
+    if( $troubleshoot && $scriptVerboseLevel >= 3 )
     {
         $cli->output( "Subtree Count: ". count( $subTree ) ."\n" );
         $cli->output( print_r( $subTree ) );
@@ -169,8 +190,11 @@ while ( $offset < $totalFileCount )
             $nodePathName = $nodeDataMap[ $attributeIdentifier ]->content();
             $nodePastPathName = $nodePastDataMap[ $attributeIdentifier ]->content();
 
+            /** Debug verbose output **/
+
             if( $verbose )
             {
+                $cli->output( "Found! S3 File needing to be renamed: " . $object->name() . "\n" );
                 $cli->output( "Current Path Name: " . $nodePathName . "\n" );
                 $cli->output( "Past Path Name: " . $nodePastPathName . "\n\n" );
             }
@@ -179,6 +203,8 @@ while ( $offset < $totalFileCount )
 
             if ( $nodePathName != $nodePastPathName  )
             {
+                /** Connect to aws s3 service **/
+
                 $awsS3 = new AmazonS3();
 
                 /** Copy object path from old version to new version **/
@@ -192,32 +218,33 @@ while ( $offset < $totalFileCount )
                                   'filename' => $nodePathName )
                 );
 
-                if( $verbose )
-                {
-                    $cli->output( "Copy Object Responce: ". print_r( $response ) . "\n");
-                }
-
                 /** Only delete old s3 file object if copy is a success **/
 
                 if( $response->isOK() )
                 {
+                    /** Debug verbose output **/
+
                     if( $verbose )
                     {
-                        $cli->output( "Removing old version file path name on AWS S3:'$nodePastPathName'\n");
+                        $cli->output( "Copy file path name on AWS S3 successfull! New path: '$nodePathName'\n");
                     }
 
                     $delete = $awsS3->delete_object( $awsS3Bucket, $nodePastPathName );
 
-                    if( $verbose )
+                    /** Optional debug output **/
+
+                    if( $troubleshoot && $scriptVerboseLevel >= 3 )
                     {
-                        $cli->output( "Delete Object Responce: ". print_r( $delete ) . "\n");
+                        $cli->output( "Delete Object Responce: \n". print_r( $delete ) . "\n");
                     }
 
                     if( $delete->isOK() )
                     {
+                        /** Debug verbose output **/
+
                         if( $verbose )
                         {
-                            $cli->output( "Delete Object on AWS S3 Successfull.\n");
+                            $cli->output( "Delete Object on AWS S3 Successfull. '$nodePastPathName'\n");
                         }
 
                         /** Only modify object if s3 file has been renamed Publish new object with renamed attribute checked **/
@@ -228,17 +255,38 @@ while ( $offset < $totalFileCount )
 
                         $result = eZContentFunctions::updateAndPublishObject( $object, $params );
 
+                        /** Debug verbose output **/
+
                         if( $verbose )
                         {
                             $cli->output( "Publishing new large_file object version with $renameAtributeIdentifier attribute checked\n");
                         }
+
+                        /** Iterate cli script progress tracker **/
+                        $script->iterate( $cli, $status );
                     }
+                }
+                else
+                {
+                    /** Debug verbose output **/
+
+                    if( $verbose )
+                    {
+                        $cli->output( "Failure! S3 File failed to be renamed: " . $object->name() . "\n" );
+                        $cli->output( "Current Path Name: " . $nodePathName . "\n" );
+                        $cli->output( "Past Path Name: " . $nodePastPathName . "\n\n" );
+                    }
+
+                    /** Optional debug output **/
+
+                    if( $troubleshoot && $scriptVerboseLevel >= 3 )
+                    {
+                        $cli->output( "Copy Object Responce: \n" . print_r( $response ) );
+                    }
+
                 }
             }
         }
-
-        /** Iterate cli script progress tracker **/
-        $script->iterate( $cli, $status );
     }
 
     /** Iterate fetch function offset and continue **/
