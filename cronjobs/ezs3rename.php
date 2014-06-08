bd87084acea2afa66ce64aa14e7ea6a15cc63d36
Brookins Consulting <info@brookinsconsulting.com>
2014-06-07 20:09:46 -0500

    Updated: Added support in cronjob for min feature usage. Minor comment improvements

--- cronjobs/ezs3rename.php
+++ cronjobs/ezs3rename.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * File containing the ezs3renameworker.php cronjob.
+ * File containing the ezs3rename.php cronjob.
  *
  * @copyright Copyright (C) 1999 - 2015 Brookins Consulting. All rights reserved.
  * @copyright Copyright (C) 2013 - 2015 Think Creative. All rights reserved.
  * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2 (or later)
- * @version 0.0.2
+ * @version 0.0.3
  * @package ezs3upload
  */
 
 $parentNodeID = eZINI::instance( 's3.ini' )->variable( 'S3Settings', 'ParentNodeID' );
-$hoursAgo = eZINI::instance( 's3.ini' )->variable( 'S3Settings', 'HoursAgo' );
+$minsAgo = eZINI::instance( 's3.ini' )->variable( 'S3Settings', 'MinsAgo' );
 
 // General cronjob part options
 $phpBin = '/usr/bin/php -d memory_limit=-1 ';
 $generatorWorkerScript = 'extension/ezs3upload/bin/php/ezs3rename.php';
-$options = '--parent-node=' . $parentNodeID . ' --hours=' . $hoursAgo;
+$options = '--parent-node=' . $parentNodeID . ' --min=' . $minsAgo;
 $result = false;
 
 // Run cronjob script command
